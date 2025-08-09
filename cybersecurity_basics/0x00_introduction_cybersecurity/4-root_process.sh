ce01b43c5d8fd3b25b7d5f7d126464d9ebd3a1c5
gunelsj <gunelsj@code.edu.az>
2025-08-09 07:37:26 -0400

    Fix script to match required format

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,19 +1,2 @@
-#!/bin/bash
-
-user=$1
-
-if [ -z "$user" ]; then
-    echo "İstifadəçi adını 1-ci argument kimi verin!"
-    exit 1
-fi
-
-ps -u "$user" -o user,pid,ppid,vsz,rss,stat,start,time,cmd | grep -v " 0 0 "#!/bin/bash
-
-user=$1
-
-if [ -z "$user" ]; then
-    echo "İstifadəçi adını 1-ci argument kimi verin!"
-    exit 1
-fi
-
-ps -u "$user" -o user,pid,ppid,vsz,rss,stat,start,time,cmd | grep -v " 0 0 "
+##!/bin/bash
+ps -u "$1" -o user,pid,ppid,vsz,rss,stat,start,time,cmd | grep -v " 0 0 "1~#!/bin/bash
