54931a718e49786512f4df432c74dab2a47823f9
gunelsj <gunelsj@code.edu.az>
2025-08-09 07:08:25 -0400

    Add script to list processes by user

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# Check if user argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <username>"
-    exit 1
-fi
+ps -u "$1" -o user,pid,pcpu,pmem,vsz,rss,tty,stat,start,time,cmd | grep -v " 0 0 "
 
-USER=$1
-
-# Show processes for the given user, excluding VSZ=0 and RSS=0
-ps -u "$USER" --no-headers -o user,pid,pcpu,pmem,vsz,rss,stat,start,time,cmd | grep -v " 0 0 "
